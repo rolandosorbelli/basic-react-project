225964cbcf8e422394b96855b24d7b7b3a9caa03
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-11-03 11:50:36 +0000

    Changed rendered text

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 class App extends Component {
 	render(){
 		return(
-			<div>React App</div>
+			<div>This app has been created manually without the React auto-generation</div>
 		);
 	}
 }
